d257591d3c0ec8ca6c9e405721be0ef64609f5e4
daria_smyr <dasha.smyr@gmail.com>
2025-01-29 22:53:42 +0300

    Added logger and db initiation if fts.go

---
+++ cmd/fts.go
@@ -0,0 +1,64 @@
+package fts
+
+import (
+	"fts-hw/config"
+	"fts-hw/internal/storage/leveldb"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("fts", "env", cfg.Env)
+
+	db, err := leveldb.New(cfg.StoragePath)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	log.Info("Database initialised", db)
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	// Waiting for SIGINT (pkill -2) or SIGTERM
+	<-stop
+
+	// initiate graceful shutdown
+	db.Close()
+	log.Info("Gracefully stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
