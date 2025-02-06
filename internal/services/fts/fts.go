edb8180471a6033a722cf171e4b872fd8b9eb5be
daria_smyr <dasha.smyr@gmail.com>
2025-02-06 17:12:32 +0300

    Add tokenize and filterStopWords methods

--- internal/services/fts/fts.go
+++ internal/services/fts/fts.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log/slog"
 	"strings"
+	"unicode"
 )
 
 type FTS struct {
@@ -38,12 +39,85 @@ func New(
 	}
 }
 
+var stopWords = map[string]struct{}{
+	"a":       {},
+	"an":      {},
+	"and":     {},
+	"are":     {},
+	"as":      {},
+	"at":      {},
+	"be":      {},
+	"but":     {},
+	"by":      {},
+	"for":     {},
+	"if":      {},
+	"in":      {},
+	"into":    {},
+	"is":      {},
+	"it":      {},
+	"no":      {},
+	"not":     {},
+	"of":      {},
+	"on":      {},
+	"or":      {},
+	"such":    {},
+	"that":    {},
+	"the":     {},
+	"their":   {},
+	"then":    {},
+	"there":   {},
+	"these":   {},
+	"they":    {},
+	"this":    {},
+	"to":      {},
+	"was":     {},
+	"were":    {},
+	"will":    {},
+	"with":    {},
+	"i":       {},
+	"me":      {},
+	"my":      {},
+	"mine":    {},
+	"we":      {},
+	"us":      {},
+	"our":     {},
+	"ours":    {},
+	"you":     {},
+	"your":    {},
+	"yours":   {},
+	"he":      {},
+	"him":     {},
+	"his":     {},
+	"she":     {},
+	"her":     {},
+	"hers":    {},
+	"himself": {},
+	"herself": {},
+}
+
 func (fts *FTS) PreprocessText(content string) []string {
 	// TODO: Add tokenization and stemming
 
 	words := strings.Fields(content)
 
 	return words
+}
+
+func (fts *FTS) tokenize(content string) []string {
+	return strings.FieldsFunc(content, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+}
+
+func (ftx *FTS) filterStopWords(tokens []string) []string {
+	filteredWords := make([]string, 0, len(tokens))
+	for _, token := range tokens {
+		if _, ok := stopWords[token]; !ok {
+			filteredWords = append(filteredWords, token)
+		}
+	}
+
+	return filteredWords
 }
 
 func (fts *FTS) AddDocument(ctx context.Context, content string) (int, error) {
